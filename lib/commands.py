74774748c562f526348a64c25ca6dd93bca541cf
Reed Essick <reed.essick@ligo.org>
2016-07-06 15:59:05 -0700

    added a module to construct command lines for jobs

---
+++ lib/commands.py
@@ -0,0 +1,9 @@
+description = "a module that builds command lines for OmegaScan processes"
+
+def omegaScanCommand( exe, exeConfig, frameDir ):
+    """
+    EXAMPLE COMMAND LINE
+    """
+    raise NotImplementedError
+
+    return cmd
